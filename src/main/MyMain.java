ba86b08c16b13365775b607b22399eb1eea275de
divaibhav <divaibhav@gmail.com>
2020-03-06 15:22:58 +0530

    added comments

--- src/main/MyMain.java
+++ src/main/MyMain.java
@@ -8,23 +8,28 @@ package main;
 
 import myqueue.MyCircularQueue;
 import node.Node;
-
+// a driver class to test MyCircularQueue implementation
 public class MyMain {
     public static void main(String[] args) {
+        // creating a circular queue i.e. creating object of MyCircularQueue
         MyCircularQueue circularQueue = new MyCircularQueue();
+        // inserting elements in queue
         circularQueue.enqueue(200);
         circularQueue.enqueue(400);
         circularQueue.enqueue(677);
 
+        // getting front element
         Node peek = circularQueue.peek();
         if (peek != null) {
             System.out.println(peek.getData() + " front of queue");
         }
+        // removing front element
         Node delete = circularQueue.dequeue();
         if (delete != null) {
             System.out.println(delete.getData() + "  deleted front of queue");
         }
 
+        // getting front element agian
         peek = circularQueue.peek();
         if (peek != null) {
             System.out.println(peek.getData() + " front of queue");
