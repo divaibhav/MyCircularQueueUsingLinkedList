df57f14285212d2d322fe7be4ceb07210f7b9252
divaibhav <divaibhav@gmail.com>
2020-03-06 14:08:54 +0530

    created class MyCircularQueue.java

---
+++ src/myqueue/MyCircularQueue.java
@@ -0,0 +1,16 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: Vaibhav
+ *  Date: 06-Mar-20
+ *  Time: 2:06 PM
+ */
+package myqueue;
+
+import node.Node;
+
+public class MyCircularQueue {
+    //taking only tail reference
+    Node rear;
+
+
+}
